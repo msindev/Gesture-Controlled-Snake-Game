fe544120ad5f07d6e9c4dacca6a00820c47d445b
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-25 00:04:35 +0530

    Mask the green colour and ouput the masked frame

--- object-detection.py
+++ object-detection.py
@@ -17,3 +17,17 @@ pts = deque(maxlen = buffer)
 video_capture = cv2.VideoCapture(0)
 
 time.sleep(2)
+
+while True:
+    ret, frame = video_capture.read()
+    frame = cv.flip(frame,1)
+    blurred_frame = cv2.GaussianBlur(frame, (10,10), 0)
+    hsv_converted_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv_converted_frame, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations = 2)
+    mask = cv2.dilate(mask, None, iterations = 2)
+
+    cv2.imshow('Masked Output', mask)
+
+    cnts = cv2.findContours(mask.copy())
