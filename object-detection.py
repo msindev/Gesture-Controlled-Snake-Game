19f3eeff65fa6117976713ae539d05f7b5d6b305
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-25 00:04:06 +0530

    Add constants and import necessary modules

--- object-detection.py
+++ object-detection.py
@@ -1,2 +1,19 @@
 ''' This script detects a object of specified object colour from the webcam video feed.
 Using OpenCV library for vision tasks and HSV color space for detecting object of given specific color.'''
+
+#Import necessary modules
+import cv2
+import numpy as np
+from collections import deque
+import time
+
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+
+buffer = 50
+
+pts = deque(maxlen = buffer)
+
+video_capture = cv2.VideoCapture(0)
+
+time.sleep(2)
