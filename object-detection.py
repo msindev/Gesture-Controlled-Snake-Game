e8301b0deb0ba9db7752d1320983d4174867ff85
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-24 23:35:25 +0530

    Create object detection module for detecting objects from HSV space.

---
+++ object-detection.py
@@ -0,0 +1,2 @@
+''' This script detects a object of specified object colour from the webcam video feed.
+Using OpenCV library for vision tasks and HSV color space for detecting object of given specific color.'''
