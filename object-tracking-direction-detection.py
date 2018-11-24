af8c0dd34e7d3246040695eb24d22040b9572944
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-25 01:40:19 +0530

    Add code for masking and finding contours

--- object-tracking-direction-detection.py
+++ object-tracking-direction-detection.py
@@ -19,3 +19,17 @@ direction = ''
 video_capture = cv2.VideoCapture(0)
 
 time.sleep(2)
+
+while True:
+    ret, frame = video_capture.read()
+    frame = imutils.resize(frame, width = 600)
+    blurred_frame = cv2.GaussianBlur(frame, (5,5), 0)
+    hsv_converted_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv_converted_frame, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations = 2)
+    mask = cv2.dilate(mask, None, iterations = 2)
+
+    _,cnts,_ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    center = None
