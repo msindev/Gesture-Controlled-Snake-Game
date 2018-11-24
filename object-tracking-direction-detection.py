e55359ad017045fecf4c82932767ee43415b196a
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-25 03:01:06 +0530

    Working code for direction detection

--- object-tracking-direction-detection.py
+++ object-tracking-direction-detection.py
@@ -10,9 +10,10 @@ import time
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
 
-buffer = 32
+buffer = 20
 
-pts = deque[buffer]
+pts = deque(maxlen = buffer)
+counter = 0
 (dX, dY) = (0, 0)
 direction = ''
 
@@ -22,6 +23,7 @@ time.sleep(2)
 
 while True:
     ret, frame = video_capture.read()
+    frame = cv2.flip(frame,1)
     frame = imutils.resize(frame, width = 600)
     blurred_frame = cv2.GaussianBlur(frame, (5,5), 0)
     hsv_converted_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
@@ -33,3 +35,45 @@ while True:
     _,cnts,_ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     center = None
+
+    if(len(cnts) > 0):
+        c = max(cnts, key = cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
+            cv2.circle(frame, center, 5, (0,255,255), -1)
+            pts.appendleft(center)
+
+    for i in np.arange(1, len(pts)):
+        if(pts[i-1] == None or pts[i] == None):
+            continue
+
+        if counter >= 10 and i == 1 and pts[-10] is not None:
+            dX = pts[-10][0] - pts[i][0]
+            dY = pts[-10][1] - pts[i][1]
+            (dirX, dirY) = ('', '')
+
+            if np.abs(dX) > 100:
+                dirX = 'West' if np.sign(dX) == 1 else 'East'
+
+            if np.abs(dY) > 100:
+                dirY = 'North' if np.sign(dY) == 1 else 'South'
+
+            direction = dirX if dirX != '' else dirY
+
+        thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    cv2.putText(frame, direction, (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+
+    cv2.imshow('Window- Direction Detection', frame)
+    key = cv2.waitKey(1) & 0xFF
+    counter += 1
+
+    if(key == ord('q')):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
